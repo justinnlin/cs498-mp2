febb13122ccba46cda5e96408ab35eb4f662d6e5
justin <justin890624@gmail.com>
2024-01-27 09:28:22 -0800

    first commit

---
+++ serve.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def serve():
+    # Stress the CPU to the specified usage
+    if request.method == 'POST':
+        # Create a subprocess to run the stress_cpu.py script
+        subprocess.Popen(["python3", "stress_cpu.py"])
+        return jsonify(message="CPU stress test start"),202
+
+    elif request.method == 'GET':
+        hostname = socket.gethostname()
+        return socket.gethostbyname(hostname)
+
+if __name__ == "__main__":
+    # Run the Flask app on host 0.0.0.0 (accessible from outside) and port 8000
+    app.run(debug=True,host='0.0.0.0', port=8000)
