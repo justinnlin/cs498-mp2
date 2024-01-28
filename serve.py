b0d30bbdd8d4509d1e9b240b52d23ce83607d0d1
justinnlin <140692487+justinnlin@users.noreply.github.com>
2024-01-27 21:40:01 -0800

    Update serve.py

--- serve.py
+++ serve.py
@@ -16,5 +16,5 @@ def serve():
         return socket.gethostbyname(hostname)
 
 if __name__ == "__main__":
-    # Run the Flask app on host 0.0.0.0 (accessible from outside) and port 8000
-    app.run(debug=True,host='0.0.0.0', port=8000)
+    # Run the Flask app on host 0.0.0.0 (accessible from outside) and port 5000
+    app.run(debug=True,host='0.0.0.0', port=5000)
