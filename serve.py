d986f9c5e62d628de34e993206f96de72d752002
justinnlin <140692487+justinnlin@users.noreply.github.com>
2024-01-27 22:46:33 -0800

    Update serve.py

--- serve.py
+++ serve.py
@@ -3,12 +3,10 @@ import socket
 import subprocess
 
 app = Flask(__name__)
-seed=0
 
 @app.route("/", methods=["GET", "POST"])
 def serve():
     # Stress the CPU to the specified usage
-    globe seed
     if request.method == 'POST':
         # Create a subprocess to run the stress_cpu.py script
         subprocess.Popen(["python3", "stress_cpu.py"])
@@ -16,10 +14,6 @@ def serve():
 
     elif request.method == 'GET':
         return socket.gethostbyname()
-
-if __name__ == "__main__":
-    # Run the Flask app on host 0.0.0.0 (accessible from outside) and port 5000
-    app.run(debug=True,host='0.0.0.0', port=5000)
 
 if __name__ == "__main__":
     # Run the Flask app on host 0.0.0.0 (accessible from outside) and port 5000
